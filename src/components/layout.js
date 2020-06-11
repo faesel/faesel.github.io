f38043471d39bb7bd791be69dbc85b367e39c4f0
Faesel Saeed <faesel.saeed@plumguide.com>
2020-06-11 22:29:49 +0100

    added footer and header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 const Layout = (props) => {
     return (
         <div>
+            <Header></Header>
             {props.children}
+            <Footer></Footer>
         </div>
     )
 }
