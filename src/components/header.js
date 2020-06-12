b5e44954af6103c0c776d5849c2c91b4269b3e9b
Faesel Saeed <faesel.saeed@plumguide.com>
2020-06-12 08:50:53 +0100

    added header and layout styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import './header.module.scss'
+import headerStyles from './header.module.scss'
 
 const Header = () => {
     return (
-        <header>
-            <h1>Faesel Saeed</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link className={headerStyles.title} to="/">
+                    Faesel Saeed
+                </Link>
+            </h1>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link className={headerStyles.link} to="/">Home</Link></li>
+                    <li><Link className={headerStyles.link} to="/blog">Blog</Link></li>
+                    <li><Link className={headerStyles.link} to="/about">About</Link></li>
+                    <li><Link className={headerStyles.link} to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
