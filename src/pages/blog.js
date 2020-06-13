0494d2585984e33c8987f86d10dde3f7775af643
Faesel Saeed <faesel.saeed@plumguide.com>
2020-06-13 16:03:06 +0100

    added some styling for blog items

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Layout from '../components/layout'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 
+import blogStyles from './blog.module.scss'
+
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
         query blogExtracts {
@@ -25,15 +27,16 @@ const BlogPage = () => {
     return (
         <Layout>
             <h1>Blog</h1>
-            <ol>
+            <ol className={blogStyles.posts}>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
-                        <li>
+                        <li className={blogStyles.post}>
                             <Link to={ `/blog/${edge.node.fields.slug}`}>
                                 <h2>{edge.node.frontmatter.title}</h2>
                                 <p>{edge.node.frontmatter.date}</p>
+                                <p>{edge.node.excerpt}</p>
                             </Link>
-                            <p>{edge.node.excerpt}</p>
+                            
                         </li>
                     )
                 })}
