3d1e2d3fa223af9afa09549cd49f5815cdcb1546
Faesel Saeed <faesel.saeed@plumguide.com>
2020-06-13 21:51:24 +0100

    added helmet to the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 const IndexPage = () => {
   return (
     <Layout>
+      <Head></Head>
       <h1>Hello,</h1>
       <h2>Im Faesel Saeed</h2>
       <p>
