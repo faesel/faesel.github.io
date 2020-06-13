c7116004523024f697f298885b1c3650d7f56da6
Faesel Saeed <faesel.saeed@plumguide.com>
2020-06-13 09:58:49 +0100

    added blog template

---
+++ src/templates/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Blog = () => {
+    return (
+        <Layout>
+            THis is the blog body
+        </Layout>
+    ) 
+}
+
+export default Blog
