f21cc094042f259742a3e97d929e062a5fe1311a
Faesel Saeed <faesel.saeed@plumguide.com>
2020-07-08 08:30:46 +0100

    Updates

--- component---src-pages-index-js-a521642b4c22484fe62f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{RXBc:function(e,l,a){"use strict";a.r(l);var t=a("q1tI"),n=a.n(t),c=a("Bl7J"),o=a("hIod");l.default=function(){return n.a.createElement(c.a,null,n.a.createElement(o.a,{title:"Home"}),n.a.createElement("h1",null,"Hello."),n.a.createElement("p",null,"I'm Faesel Saeed, a full-stack developer."))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-a521642b4c22484fe62f.js.map
