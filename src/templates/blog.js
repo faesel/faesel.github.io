325ab0dd8bb13e9c8ca8e8bb2cee5bd506a81f01
Faesel Saeed <faesel.saeed@plumguide.com>
2021-01-24 12:07:31 +0000

    removed unexpected tab

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -25,7 +25,7 @@ export const query = graphql`
             title,
             slug
             tags
-    				hero {
+            hero {
               file {
                 url
               }
