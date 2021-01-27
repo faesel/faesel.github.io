e1a872b24754ff5e7122aafa4d9ed0efa59b58f3
Faesel Saeed <faesel.saeed@plumguide.com>
2021-01-27 14:12:59 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6a3e16e809c8b9f2ff6d.js"
+    "url": "webpack-runtime-73bba72608274a1dd736.js"
   },
   {
     "url": "framework-f22f3b8b99ca8be9eba5.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "92f000455a0a1074cd8d6f40db3d30c9"
+    "revision": "379f31e764ac059c71095dbec14db278"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e5cb9e0c77a823b86dc2.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "89d8f0e56e44821f657cfc10651ee1e3"
+    "revision": "52e943053aee430d1ba5455de37b3e29"
   },
   {
     "url": "manifest.webmanifest",
