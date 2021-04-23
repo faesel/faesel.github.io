79a3821703694cf50879b133a2c648fe9bf1e087
Faesel Saeed <faesel.saeed@plumguide.com>
2021-04-23 15:45:12 +0100

    added urls to related links

--- src/components/relatedArticle.js
+++ src/components/relatedArticle.js
@@ -4,7 +4,7 @@ import relatedArticleStyle from "./relatedArticle.module.scss"
 const RelatedArticle = ({ title, url, heroImageUrl, heroImageAlt }) => {
   return (
     <div className={relatedArticleStyle.box}>
-      <a href={url}>
+      <a rel="me" className="u-url" href={url}>
         <img
           className={relatedArticleStyle.boximage}
           height={100}
